7c868b06430c0d264317818d8667d4d021a33a6a
yannick-ashu <Ashuyannick691@gmail.com>
2022-11-22 17:39:44 +0100

    project

---
+++ script.js
@@ -0,0 +1,9 @@
+let carrierObject = Object
+    mtn = 'MTN'
+    orange = 'ORANGE'
+    nexttel = 'NEXTTEL'
+    cantel = 'CAMTEL'
+
+
+    let number = document.getElementById('phoneNumber')
+    console.log(number)
